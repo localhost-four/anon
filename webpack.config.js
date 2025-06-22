60dfa3d82b66273003e729fd2ad02259642b5de0
staffuser <119116574+staffuser@users.noreply.github.com>
2025-06-22 18:44:25 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: '/anon/index.html',  // точка входа вашего приложения
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '/anon/bundle.js', // название основного бандла
+    publicPath: 'anon', // путь на GitHub Pages
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader', // трансляция JavaScript с помощью Babel
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],  // обрабатываем стили
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource', // обработка изображений
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // Очистка старых файлов сборки
+    new HtmlWebpackPlugin({
+      template: '/anon/index.html', // файл-шаблон для генерации index.html
+    }),
+  ],
+  devServer: {
+    static: 'about', // сервирует файлы из папки dist для разработки
+    historyApiFallback: true, // позволяет корректно работать с маршрутизацией SPA
+  },
+  devtool: 'source-map', // создание исходных карт для отладки
+};
